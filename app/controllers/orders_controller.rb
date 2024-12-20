e9e7db2d82ad0573fea88c580871440af1f37f67
naota7118 <naota7118@gmail.com>
2024-12-20 18:31:34 +0900

    注文用コントローラにnewアクションとcreateアクションを追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,19 @@
 class OrdersController < ApplicationController
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    binding.pry
+    @order = Order.build(order_params)
+    if @order.save
+      redirect_to root_url
+    end
+  end
+
+  private
+    def order_params
+      params.require(:order).permit(:curry, :spiceness, :ricesize, :price)
+    end
 end

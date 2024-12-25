995b83004c884d84b9b3b850a5bb6324c4a8a681
naota7118 <naota7118@gmail.com>
2024-12-25 19:12:02 +0900

    カスタマー登録失敗時と成功時のテスト追加

--- test/integration/customers_signup_test.rb
+++ test/integration/customers_signup_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class CustomersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid signup customer" do
+    get signup_path
+    assert_response :success
+    post customers_path, params: { customer: { name: "",
+                                               email: "customer@invalid",
+                                               password: "foo",
+                                               password_confirmation: "bar" } }
+    assert_response :unprocessable_entity
+    assert_template 'customers/new'
+    assert_template partial: 'shared/_error_messages'
+  end
+
+  test "valid signup customer" do
+    get signup_path
+    assert_response :success
+    post customers_path, params: { customer: { name: "james",
+                                               email: "james@valid",
+                                               password: "password",
+                                               password_confirmation: "password" } }
+    follow_redirect!
+    assert_template 'customers/show'
+    assert_template 'layouts/application'
+  end
 end

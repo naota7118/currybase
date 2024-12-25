fa7fe2805d3ba9922b7eb3ff6b95698ef3117537
naota7118 <naota7118@gmail.com>
2024-12-25 12:08:24 +0900

    add:注文内容の更新処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -43,6 +43,15 @@ class OrdersController < ApplicationController
     @spicenesses = Spiceness.all
   end
 
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      redirect_to @order
+    else
+      render 'edit', status: 'unprocessable_entity'
+    end
+  end
+
   def destroy
     @order = Order.find(params[:id])
     @order.destroy

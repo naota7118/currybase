f9202111e7f05cda4ac6c7954b6482f63daeaa83
naota7118 <naota7118@gmail.com>
2024-12-25 13:37:26 +0900

    customersテーブルにemailカラムとpassword_digestカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_25_032445) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_25_043353) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_25_032445) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
   end
 
   create_table "orders", force: :cascade do |t|

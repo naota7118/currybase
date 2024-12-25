05f480a65db9e2624acd71acb2b8822b4b9f9e5f
naota7118 <naota7118@gmail.com>
2024-12-25 13:43:12 +0900

    customerテーブルのpasswordにバリデーションを追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,5 @@ class Customer < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
